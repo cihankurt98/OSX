ef187702e485bdb6df583697f4b70064a2d2052e
Cihan <cihankurt1998@gmail.com>
2018-06-06 14:52:57 +0200

    msq fail
    fail

--- CGI/web.cpp
+++ CGI/web.cpp
@@ -37,6 +37,7 @@ int main()
 	       "Content-Type:text/html;charset=iso-8859-1", 13, 10);
 	printf("<TITLE>CGI</TITLE>\n");
 	printf("<H3>CGI</H3>\n");
+	printf("<H4>Klik op een button om de waardes te zien.</H4>\n");
 	printf("<meta http-equiv= \"refresh\" content=\"1\">");
 	data = getenv("QUERY_STRING");
 	if (data == NULL)
@@ -81,19 +82,11 @@ int main()
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_one, sizeof(led_one), &transferred2, NOTIMEOUT);
 		std::cout << "Button A: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "Button A: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[3] & BUTTON_B_OFFSET && input->inputarray[0] == 0)
 	{
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_two, sizeof(led_two), &transferred2, NOTIMEOUT);
 		std::cout << "Button B: " << "Pressed<br>";
-	}
-	else
-	{
-		std::cout << "Button B: " << "Not Pressed<br>";
 	}
 
 	if (input->inputarray[3] & BUTTON_X_OFFSET && input->inputarray[0] == 0)
@@ -101,82 +94,46 @@ int main()
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_three, sizeof(led_three), &transferred2, NOTIMEOUT);
 		std::cout << "Button X: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "Button X: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[3] & BUTTON_Y_OFFSET && input->inputarray[0] == 0)
 	{
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_four, sizeof(led_four), &transferred2, NOTIMEOUT);
 		std::cout << "Button Y: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "Button Y: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[2] & BUTTON_B_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Button Back: " << "Pressed<br>";
-	}
-	else
-	{
-		std::cout << "Button Back: " << "Not Pressed<br>";
 	}
 
 	if (input->inputarray[2] & BUTTON_A_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Button Start: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "Button Start: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[3] & BUTTON_XBOX_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Button Xbox: " << "Triggered<br>";
-	}
-	else
-	{
-		std::cout << "Button Xbox: " << "Not triggered<br>";
 	}
 
 	if (input->inputarray[3] & BUTTON_TRIGGER_R1_OFFSET && input->inputarray[0] == 0 )
 	{
 		std::cout << "Trigger R1: " << "Triggered<br>";
 	}
-	else
-	{
-		std::cout << "Trigger R1: " << "Not triggered<br>";
-	}
 
 	if (input->inputarray[3] & BUTTON_TRIGGER_L1_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Trigger L1: " << "Triggered<br>";
-	}
-	else
-	{
-		std::cout << "Trigger L1: " << "Not triggered<br>";
 	}
 
 	if (input->inputarray[2] & BUTTON_LEFT_JOYSTICK_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Left joystick: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "Left joystick: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[2] & BUTTON_RIGHT_JOYSTICK_OFFSET && input->inputarray[0] == 0)
 	{
 		std::cout << "Right joystick: " << "Pressed<br>";
-	}
-	else
-	{
-		std::cout << "Right joystick: " << "Not Pressed<br>";
 	}
 
 	if (input->inputarray[2] & BUTTON_DPAD_UP_OFFSET && input->inputarray[0] == 0)
@@ -184,19 +141,11 @@ int main()
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_rotating, sizeof(led_rotating), &transferred, NOTIMEOUT);
 		std::cout << "D-Pad Up: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "D-Pad Up: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[2] & BUTTON_DPAD_DOWN_OFFSET && input->inputarray[0] == 0)
 	{
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_blinking, sizeof(led_blinking), &transferred, NOTIMEOUT);
 		std::cout << "D-Pad Down: " << "Pressed<br>";
-	}
-	else
-	{
-		std::cout << "D-Pad Down: " << "Not Pressed<br>";
 	}
 
 	if (input->inputarray[2] & BUTTON_DPAD_LEFT_OFFSET && input->inputarray[0] == 0)
@@ -204,19 +153,11 @@ int main()
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_alternating, sizeof(led_alternating), &transferred, NOTIMEOUT);
 		std::cout << "D-Pad Left: " << "Pressed<br>";
 	}
-	else
-	{
-		std::cout << "D-Pad Left: " << "Not Pressed<br>";
-	}
 
 	if (input->inputarray[2] & BUTTON_DPAD_RIGHT_OFFSET && input->inputarray[0] == 0)
 	{
 		//libusb_interrupt_transfer(h, ENDPOINTOUT, led_slowblinking, sizeof(led_slowblinking), &transferred, NOTIMEOUT);
 		std::cout << "D-Pad Right: " << "Pressed<br>";
-	}
-	else
-	{
-		std::cout << "D-Pad Right: " << "Not Pressed<br>";
 	}
 
 	sem_wait(sem);
